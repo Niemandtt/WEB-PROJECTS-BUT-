864676a0ade5584550ae13bb0081179f1d85d002
Niemandtt <annapou2006@gmail.com>
2025-09-29 20:07:12 +0500

    Delete task/script.js

--- task/script.js
+++
@@ -1,8 +0,0 @@
-let num = parseInt(prompt("Введите число для вычисления факториала:"));
-let factorial = 1;
-
-for (let i = 1; i <= num; i++) {
-  factorial *= i;
-}
-
-document.write("Факториал числа " + num + " равен " + factorial);
